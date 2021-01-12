70547c07e9f3ed940e8a1b0bfad3bbf69f26f0f3
mhossain <m.hossain@blackcover.fr>
2021-01-12 21:46:03 +0100

    initialize the project

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container class="">
+    <v-row class="">
+      <v-col cols="12">
+        <h1>Bienvenue dans Niagara</h1>
+        <v-select
+            :items="items"
+            filled
+            label="Filtre"
+            width="8"
+            height="12"
+        ></v-select>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    data: () => ({
+    }),
+  }
+</script>
